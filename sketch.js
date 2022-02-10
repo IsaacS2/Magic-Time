c3061f1c6d5bdac22300147be5c6b20511527b34
BroIsaac2Hud <79726528+BroIsaac2Hud@users.noreply.github.com>
2022-02-09 22:33:34 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     window.alert("Magic Time; This piece is based on magic and turning average, ordinary shapes into colorful pieces of art. There are 3 shapes that move around the screen that the user can interact with to make more lively.");
-    window.alert("Press/hold the left mouse button to add magic sparkles from the tip of the wand. Pressing on or near the circle, triangle, or square when they are grey will cause them to become colorful and release sparkles. Depending on the shapes activated, their movements or forms may change as well.");
+    window.alert("Press/hold the left mouse button to add magic sparkles from the tip of the wand. Pressing on or near the circle, triangle, or square when they are grey will cause them to become colorful and release sparkles. Depending on the shapes activated, their movements or forms may change as well. When the circle and triangle leave the screen and return, they revert to their original forms, and the user can click on them again to alter them once more; the same applies to the square when it hits the edges of the screen.");
     noCursor();
     hatty = new hat(windowWidth * (1/2), windowHeight * (9/10), 500, 200);
     magicCircle = new megaCircle(-62, random(62, windowHeight - 62), 124, 233, 32, 0, 0);
